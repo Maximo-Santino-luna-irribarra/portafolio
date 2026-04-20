473b40b0d4e8166ce2cabf402e0e7e64f7c8db5f
Maximo Santino Luna Irribarra <148594458+Maximo-Santino-luna-irribarra@users.noreply.github.com>
2026-04-20 07:31:05 -0300

    .

---
+++ portfolio-astro/src/constants.ts
@@ -0,0 +1,94 @@
+export const projects = [
+    {
+        title: "App web tipo tótem",
+        desc: "App web para administración de productos.",
+        link: "https://github.com",
+    },
+    {
+        title: "App web para gestión de gym",
+        desc: "App web para administrar membresías y pagos.",
+        link: "https://github.com",
+    },
+    {
+        title: "Sala de Juegos",
+        desc: "Sistema web interactivo.",
+        link: "https://github.com",
+    },
+    {
+        title: "App mobile",
+        desc: "Dashboard con filtros y roles.",
+        link: "https://github.com",
+    },
+    ];
+
+    export const technologies = [
+    {
+        name: "JavaScript",
+        image: "portfolio-astro\src\assets\svgs\javascript.svg",
+        description: "Lenguaje base para desarrollo web moderno.",
+    },
+    {
+        name: "Node.js",
+        image: "/icons/nodejs.svg",
+        description: "Entorno para ejecutar JavaScript en el backend.",
+    },
+    {
+        name: "MongoDB",
+        image: "portfolio-astro\src\assets\svgs\mongodb.svg",
+        description: "Base de datos NoSQL orientada a documentos.",
+    },
+    {
+        name: "Astro",
+        image: "/icons/astro.svg",
+        description: "Framework moderno ideal para sitios rápidos y portfolios.",
+    },
+    {
+        name: "Git",
+        image: "portfolio-astro\src\assets\svgs\git.svg",
+        description: "Framework moderno ideal para sitios rápidos y portfolios.",
+    },
+    {
+        name: "MongoDB",
+        image: "/icons/astro.svg",
+        description: "Framework moderno ideal para sitios rápidos y portfolios.",
+    },
+    {
+        name: "TypeScript",
+        image: "/icons/astro.svg",
+        description: "Framework moderno ideal para sitios rápidos y portfolios.",
+    },
+    {
+        name: "Python",
+        image: "/icons/astro.svg",
+        description: "Framework moderno ideal para sitios rápidos y portfolios.",
+    },
+    
+];
+export const experience = [
+    {
+        company: "Artes Gráficas Phoenix",
+        period: "2023 - Actualidad",
+        role: "Operario / Soporte it en negocio gráfico / administracion",
+        description:
+        "Experiencia en trabajo operativo, organización y procesos dentro del entorno gráfico incluyen trabajos de it como instalacion de equipos mantenimiento de los mismos.",
+    },
+    {
+        company: "Proyectos personales",
+        period: "2024 - Actualidad",
+        role: "Desarrollador Full Stack",
+        description:
+        "Desarrollo de aplicaciones web y sistemas propios con foco en frontend, backend y bases de datos fullstack.",
+    },
+];
+export const education = [
+    {
+        title: "Bachiller en Sociales",
+        period: "2018 - 2023",
+        description: "Formación secundaria completa.",
+    },
+    {
+        title: "Técnico en Programación",
+        period: "2024 - 2025",
+        description: "Formación en desarrollo web y backend.",
+    },
+];
